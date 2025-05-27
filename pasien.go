56f81c99c5ecbeb43bb5d30762d31a45bd813bc3
YohanaSinaga <yohana.snga0701@gmail.com>
2025-05-27 23:01:16 +0700

    pasien.go

--- pasien.go
+++ pasien.go
@@ -1,55 +1,53 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"strings"
 )
 
-type Pasien struct {
-	Nama     string
-	Umur     int
-	Penyakit string
-	Biaya    int
+func linearSearchNama(data []Pasien, keyword string) {
+	clearScreen()
+	found := false
+	for _, p := range data {
+		if strings.EqualFold(p.Nama, keyword) {
+			fmt.Print("Pasien ditemukan:")
+			fmt.Printf("Nama: %s | Umur: %d | Penyakit: %s | Biaya: Rp%d\n\n", p.Nama, p.Umur, p.Penyakit, p.Biaya)
+			found = true
+			break
+		}
+	}
+	if !found {
+		fmt.Print("Pasien tidak ditemukan.\n")
+	}
 }
 
-var daftarPasien []Pasien
-
-func inputPasien() {
+func binarySearchUmur(data []Pasien, target int) {
 	clearScreen()
-
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Masukkan Nama Pasien: ")
-	nama, _ := reader.ReadString('\n')
-	nama = strings.TrimSpace(nama)
-
-	fmt.Print("Masukkan Umur Pasien: ")
-	var umur int
-	fmt.Scanln(&umur)
-
-	fmt.Print("Masukkan Penyakit: ")
-	penyakit, _ := reader.ReadString('\n')
-	penyakit = strings.TrimSpace(penyakit)
-
-	fmt.Print("Masukkan Biaya: ")
-	var biaya int
-	fmt.Scanln(&biaya)
-
-	baru := Pasien{Nama: nama, Umur: umur, Penyakit: penyakit, Biaya: biaya}
-	daftarPasien = append(daftarPasien, baru)
-	fmt.Print("Pasien berhasil ditambahkan!\n")
-}
-
-func tampilkanPasien() {
-	clearScreen()
-	if len(daftarPasien) == 0 {
-		fmt.Print("Belum ada data pasien.\n")
+	if len(data) == 0 {
+		fmt.Print("Tidak ada data pasien.\n")
 		return
 	}
-	fmt.Println("=== Daftar Pasien ===")
-	for i, p := range daftarPasien {
-		fmt.Printf("%d. Nama: %s | Umur: %d | Penyakit: %s | Biaya: Rp%d\n", i+1, p.Nama, p.Umur, p.Penyakit, p.Biaya)
+	selectionSortUmur(data)
+	low := 0
+	high := len(data) - 1
+	found := false
+
+	for low <= high {
+		mid := (low + high) / 2
+		if data[mid].Umur == target {
+			fmt.Print("Pasien dengan umur ditemukan:")
+			fmt.Printf("Nama: %s | Umur: %d | Penyakit: %s | Biaya: Rp%d\n\n", data[mid].Nama, data[mid].Umur, data[mid].Penyakit, data[mid].Biaya)
+			found = true
+			break
+		} else if data[mid].Umur < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
 	}
-	fmt.Print()
+	if !found {
+		fmt.Print("Tidak ada pasien dengan umur tersebut.\n")
+	}
 }
+
+
